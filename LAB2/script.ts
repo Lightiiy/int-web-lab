969b46c74b50c1c59c899f92f4f8f24424dc6075
bekierg <gabriel.bekier@comarch.com>
2023-04-14 18:32:50 +0200

    styling & dynamic lists

--- LAB2/script.ts
+++ LAB2/script.ts
@@ -12,20 +12,23 @@ class ToDoList{
     actionList: Action[];
     private listRender: Element;
     private date: string;
+    private graveyard?: Action;
+    private yard: Element;
 
     constructor(title: string){
         this.title = title;
-        this.actionList = [{action: 'test', isDone: false},
-            {action: 'test15', isDone: false},
-            {action: 'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum', isDone: false}];
+        this.date = this.getFormatedDate();
+        this.actionList = [];
+            
         this.listRender = document.createElement('ol');
         this.listRender.classList.add('to-do-list');
         this.listRender.id = (this.title);
-        document.querySelector('#list-wrapper')?.appendChild(this.listRender);
-        this.date = this.getFormatedDate();
+
+        this.yard = document.createElement('p');
+
         if(this.actionList.length !== 0)
         {
-            this.actionList.forEach(element => {
+        this.actionList.forEach(element => {
                 this.createDeleteButton(this.appendtoRenderedList(element.action));
             });
         }
@@ -42,7 +45,7 @@ class ToDoList{
     }
 
     appendtoRenderedList = (action: string): Element => {
-        const newLI = document.createElement('li')
+        const newLI = document.createElement('li');
         newLI.setAttribute('date', this.date);
         newLI.appendChild(document.createTextNode(action));
         this.listRender.appendChild(newLI);
@@ -60,6 +63,7 @@ class ToDoList{
     removeFromBothLists = (index: number) => {
         const removable = [...this.listRender.children][index];
         this.listRender.removeChild(removable);
+        this.appendToGraveyard(this.actionList[index]);
         this.actionList.splice(index, 1);
     }
     
@@ -76,36 +80,161 @@ class ToDoList{
         return today;
     }
 
+    appendToGraveyard(action: Action){
+        this.graveyard = action;
+        const grave = document.createElement('p');
+        grave.textContent=action.action;
+        grave.classList.add('grave');
+        if (this.yard.hasChildNodes()){
+            this.yard.replaceChildren(grave);
+        }
+        this.yard.appendChild(grave);
+    }
+
+    resurrect(){
+        if(this.graveyard === undefined)
+        {
+            console.error ("Can't resurrect the dirt");
+            return;
+        }
+        this.addActionToList(this.graveyard.action);
+        delete this.graveyard;
+    }
+
+    renderList(){
+        document.querySelector('#list-wrapper')?.appendChild(this.listRender);
+
+        document.querySelector('#graveyard-wrapper')?.appendChild(this.yard);
+    }
+
+    derenderList(){
+        document.querySelector('#list-wrapper')?.removeChild(this.listRender);
+
+        document.querySelector('#graveyard-wrapper')?.removeChild(this.yard);
+    }
 }
 
 
-
 //MAIN
-const mainList = new ToDoList('mainList');
-const currList = mainList;
+const mainList = new ToDoList('Main List');
+const altList = new ToDoList('Additional List');
+const listOfLists = [mainList, altList];
+const listOfListsRender: Element = document.createElement('ol');
+var currList = mainList;
+
+mainList.addActionToList('test');
+mainList.addActionToList('test2');
+mainList.addActionToList('test3');
+mainList.addActionToList('test4');
 
 
+altList.addActionToList('dupa');
+altList.addActionToList('dupa2');
+altList.addActionToList('dup3a');
+altList.addActionToList('dup4a');
+
+listOfListsRender.classList.add('list-of-lists')
+document.querySelector('.lists-wrapper')?.appendChild(listOfListsRender);
+
+
+const addListToRender = (title: string) => {
+    const newLI = document.createElement('li');
+    newLI.appendChild(document.createTextNode(title));
+    newLI.addEventListener("click", (event) =>{
+        const element = event.target;
+        const index = [...listOfListsRender.children].indexOf(element);
+        switchContext(index)
+    })
+    listOfListsRender.appendChild(newLI);
+}
+
+listOfLists.forEach( (element) => {
+    addListToRender(element.title);
+})
+
+currList.renderList();
+
+
+const switchContext = (index:number) =>{
+    currList.derenderList();
+    currList = listOfLists[index];
+    currList.renderList();
+};
+
+
+const addListToLists = (list: ToDoList) => {
+    listOfLists.push(list);
+    addListToRender(list.title);
+}
 
 const formActionAddToList = (list: ToDoList, action: string) => 
 {
     list.addActionToList(action);
 }
 
+const formListAddToLists = (title: string) => {
+    const newList = new ToDoList(title);
+    addListToLists(newList);
+    switchContext(listOfLists.length - 1);
+}
+
+
 const overrideEnter = (event: Event) => {
-    var inputEl = document.querySelector('input[name=action_input]');
-    if (event.key === 'Enter'){
-        event?.preventDefault();
-        formActionAddToList(currList, event.target.value);
+    const inputEl = document.querySelector('input[name=action_input]');
+    if (inputEl !== null) {
+        if (event.key === 'Enter'){
+            event?.preventDefault();
+            formActionAddToList(currList, event.target.value);
+            inputEl.value = '';
+        }
     }
-    
 }
 
 
 
-//ZAD Jquery.1 -> usuwanie wybranego elementu z listy
 $(function(){
-        $(document).on("click",'.delete-button', function () {
-            const index = $(this).parent().index();
-            currList.removeFromBothLists(index);
+
+    //  function toggleModal(variant: string, index: number = 0){
+    //     console.log(variant);
+    //         switch (variant)
+    //         {
+    //             case 'delete':
+    //                 $('#modal').toggleClass('hidden');
+    //                 $('#delete').toggleClass('hidden');
+    //                 $('#delete-yes').on("click", function(){
+    //                     console.log(index);
+    //                     // currList.removeFromBothLists(index);
+    //                     $('#modal').toggleClass('hidden');
+    //                     $('#delete').toggleClass('hidden');
+    //                 })
+    //                 break;
+    //             default:
+    //                 console.error('wrong variant of modal');
+
+    //         }
+    // }
+
+
+//ZAD Jquery.1 -> usuwanie wybranego elementu z listy
+//ZAD Jquery.3 -> modal przy usuwaniu elementu z listy
+    $(document).on("click",'.delete-button', function () {
+        // toggleModal('delete', $(this).parent().index());
+        const index = $(this).parent().index();
+        currList.removeFromBothLists(index);
     })
+//ZAD Jquery.2 -> kosz z funkcjonalnoscia ctrl+z
+    $(document).on("click", '.grave', function(){
+        currList.resurrect();
+        $(this).remove();
+    })
+
+    $(document).keydown(function(event){
+        if(event.which === 90 && event.ctrlKey)
+            {
+                currList.resurrect();
+                 $('.grave').remove();
+            }
+    })
+
 })
+
